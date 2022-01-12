0139e3b88f2f192d6070881a42715ff138a95dc0
Anna Tylkowska <tylkowskanna@gmail.com>
2022-01-12 12:46:05 +0100

    added styles

--- moduleList/components/ModuleItem.jsx
+++ moduleList/components/ModuleItem.jsx
@@ -6,18 +6,24 @@ export default class Module extends Component {
         console.log(this.props)
         return (
             <View style={styles.container}>
-                <View>
-                    <Text style={styles.title}>{this.props.data.module.item.name}</Text>
-                    <Text style={styles.description}>{this.props.data.module.item.description}</Text>
+                <View style={styles.itemDetails}>
+                    <View >
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{this.props.data.module.item.name}</Text>
+                            <Text style={styles.year}>{this.props.data.module.item.year}</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.description}>{this.props.data.module.item.description}</Text>
+                        </View>
+                    </View>
+                    <Text style={styles.teacher}>{this.props.data.module.item.teacher}</Text>
                 </View>
-                <View>
-                    <Text style={styles.year}>{this.props.data.module.item.year}</Text>
-                    {/* <Image
-                        style={styles.icon}
-                        source={}
-                    /> */}
-                </View>
-                <Text style={styles.teacher}>{this.props.data.module.item.teacher}</Text>
+                {/* <Image
+                    style={styles.icon}
+                    source={{
+                        uri: this.props.data.module.item.icon,
+                    }}
+                /> */}
             </View>
         );
     }
@@ -27,8 +33,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         justifyContent: 'center',
+        margin: 20,
+    },
+    itemDetails: {
+        width: 250,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     title: {
         fontSize: 30,
@@ -40,11 +54,14 @@ const styles = StyleSheet.create({
     },
     year: {
         fontSize: 36,
+        marginLeft: 10,
     },
     icon: {
         width: 50,
     },
     teacher: {
         fontSize: 24,
+        alignSelf: 'flex-end',
+        marginTop: 50,
     }
 });
